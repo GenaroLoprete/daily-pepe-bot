b3c29057054e5a3153ce8fdb1dec7d0857c33be1
GenaroLoprete <gena.loprete@gmail.com>
2021-03-14 02:36:52 -0300

    cron modify

--- pepe-bot.js
+++ pepe-bot.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 console.log(new Date());
-cron.schedule('0 18 * * *', () => {
+cron.schedule('0 10 * * *', () => {
     require('./functions/makeTweet').makeTweet();
 });
 
