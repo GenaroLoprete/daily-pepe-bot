07f567d827ea158134d24b9b16e6f92baff92e9c
GenaroLoprete <gena.loprete@gmail.com>
2021-04-07 12:08:43 -0300

    f

--- pepe-bot.js
+++ pepe-bot.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 console.log(new Date());
-cron.schedule('0 15 * * *', () => {
+cron.schedule('10 15 * * *', () => {
     require('./functions/makeTweet').makeTweet();
 });
 
